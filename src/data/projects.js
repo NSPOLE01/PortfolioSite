a8ecc46011e35a3ce7fb658c67b037b3b831aa9c
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2024-04-15 16:33:44 -0500

    Adding vandyDorm project.

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,38 +4,46 @@ import notes from "../images/notes.jpg";
 import tv from "../images/tv.jpg";
 import arduinoHeart from "../images/arduinoHeart.jpg";
 import vandyPool from "../images/VandyPool.jpg";
+import rateMyDorm from "../images/rateMyDorm.jpg";
 
 export const projectsData = [
   {
     id: 1,
+    title: "VandyDorm",
+    category: "Bolstering the way students visualize campus dorms",
+    img: rateMyDorm,
+    github: "https://github.com/NSPOLE01/ratemydorm",
+  },
+  {
+    id: 2,
     title: "VandyPool",
     category: "Helps Vanderbilt Students save $$$ on airport rides",
     img: vandyPool,
     github: "https://github.com/NSPOLE01/vandypool",
   },
   {
-    id: 2,
+    id: 3,
     title: "Credit Card Cash-Back Maximizer",
     category: "Visualize and maximize cash-back values",
     img: creditCards,
     github: "https://github.com/NSPOLE01/CashBackMaximizer",
   },
   {
-    id: 3,
+    id: 4,
     title: "React and Redux Notes Manager",
     category: "Store, edit and manage your notes easily!",
     img: notes,
     github: "https://github.com/NSPOLE01/NoteManager",
   },
   {
-    id: 4,
+    id: 5,
     title: "React TV Show Adviser",
     category: "Find TV shows you may like",
     img: tv,
     github: "https://github.com/NSPOLE01/TVShowSuggester",
   },
   {
-    id: 5,
+    id: 6,
     title: "Tachycardia and Bradycardia Diagnostic Tool",
     category: "Arduino Microcontroller Wearable Device + Python Script",
     img: arduinoHeart,
